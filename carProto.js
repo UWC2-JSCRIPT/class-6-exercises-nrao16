354a031755b5162d30f55e9b59c8b0ac065a3371
Nandini Rao [nandinirao] <nandinirao@costco.com>
2022-11-22 21:06:58 -0800

    initial class work

--- carProto.js
+++ carProto.js
@@ -4,7 +4,34 @@
  * @param {String} model
  */
 
+class Car {
+    constructor(model) {
+        this.currentSpeed = 0;
+        this.model = model;
+        this.accelerate = () => {
+            this.currentSpeed++;
+        }
+        this.brake = () => {
+            this.currentSpeed--;
+        }
+    }
+    // accelerate = () => {
+    //         this.currentSpeed++;
+    //     }
+    // brake = () => {
+    //         this.currentSpeed--;
+    //     }
+    toString = () => {
+        return (`model:${this.model}, currentSpeed:${this.currentSpeed}`);
+    }
+}
 //  Create an instance, accelerate twice, brake once, and console log the instance.toString()
+const car1 = new Car('Honda');
+car1.accelerate();
+car1.accelerate();
+car1.brake();
+console.log(car1.toString());
+console.log(car1);
 
 /**
  * ElectricCar class
@@ -13,3 +40,23 @@
  */
 
 //  Create an instance, accelerate twice, brake once, and console log the instance.toString()
+class ElectricCar extends Car {
+    constructor(model) {
+        super(model);
+    }
+
+    accelerate () {
+        super.accelerate();
+        super.accelerate();
+    }
+
+    toString() {
+        return (`model:${this.model}, currentSpeed:${this.currentSpeed}`)
+    }
+}
+const car2 = new ElectricCar('Nissan');
+car2.accelerate();
+car2.accelerate();
+car2.brake();
+console.log(car2.toString());
+console.log(car2);
--- jQueryDomCrud.js
+++ jQueryDomCrud.js
@@ -1,8 +1,45 @@
+// $(document).ready( function () {
+//   const $listItems = $('li');
+//   $listItems[2].className = 'highlight';
+
+//   const $img =$('img');
+//   const $imgColor = $img.data('color');
+//   console.log($imgColor);
+
+//   const $h1 = $('h1');
+//   console.log($h1.text('Something else'));
+//   console.log($h1.html());
+
+//   //create
+//   const $h2 = $("<h2>");
+//   $h2.text(`Here's that H2`);
+//   //get a reference
+//   const $h2ref = $("h2");
+
+//   const $main = $("main");
+//   $main.prepend($h2);
+
+//   //or
+//   //$('main').prepend($h2);
+
+//   //remove the element
+//   $img.remove();
+
+
+// });
 // Create a new <a> element containing the text "Buy Now!"
 // with an id of "cta" after the last <p>
 
+const $a = $('<a>');
+$a.text('Buy Now!');
+$a.attr('id', 'cta');
+const $p = $('p');
+$($p[$p.length-1]).parent().append($a);
+
 // Access (read) the data-color attribute of the <img>,
 // log to the console
+const $imgColor = $('img').data('color');
+console.log($imgColor);
 
 // Update the third <li> item ("Turbocharged"),
 // set the class name to "highlight"
